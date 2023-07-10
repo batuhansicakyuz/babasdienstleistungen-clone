b9508e3d1d3f81d93bc305627d36e095cb70caa2
batuhansicakyuz <batuhansicakyuz@gmail.com>
2023-07-10 12:30:41 +0200

    home added

---
+++ src/app/dropdown/dropdown.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'drop-down',
+  templateUrl: './drop-down.component.html',
+  styleUrls: ['./drop-down.component.css']
+})
+export class DropDown {
+
+}
---
+++ src/app/headersection1/headersection1.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'header-section1',
+  templateUrl: './headersection1.component.html',
+  styleUrls: ['./headersection1.component.css']
+})
+export class HeaderSection1 {
+
+  constructor() {}
+
+}
